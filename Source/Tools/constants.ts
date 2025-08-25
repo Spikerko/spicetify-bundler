7d2b7d6a734e2513a282bafe9a0d7e5734437073
SpikeNew7774 <newadm159@gmail.com>
2025-08-25 20:59:56 +0200

    Remove Unused code

--- Source/Tools/constants.ts
+++ Source/Tools/constants.ts
@@ -1,22 +1,3 @@
-import { join } from 'jsr:@std/path@1.1.2';
-
-export const isWindows = Deno.build.os === 'windows';
-
-export const HOME_PATH = (isWindows
-  ? Deno.env.get('APPDATA')
-  : join(Deno.env.get('HOME') ?? '/home/jack', '.config')) ?? "";
-
-
-export const SPICETIFY_PATH = join(HOME_PATH, 'spicetify');
-export const SPOTIFY_PATH = isWindows ? join(HOME_PATH, 'Spotify') : '/opt/spotify';
-
-export const EXTENSION_PATH = join(SPICETIFY_PATH, 'Extensions');
-export const XPUI_PATH = join(SPOTIFY_PATH, 'Apps/xpui');
-export const XPUI_EXTENSION_PATH = join(XPUI_PATH, 'extensions');
-
-export const EXTENSION_ENTRY_POINTS = [join(Deno.cwd(), 'src/index.tsx')];
-export const LIVERELOAD_JS_PATH = join(Deno.cwd(), 'builder/client/liveReload.js');
-
 export const RequirementsPromiseCheckString = `
 await new Promise(
   resolve => {
